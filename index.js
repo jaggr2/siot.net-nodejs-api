bc51d2a75aafadbb225a32a4b1404c40418583cc
roger.jaggi <roger.jaggi@blastbeatsyndicate.com>
2016-04-18 10:39:36 +0200

    updated package-info

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 'use strict';
 
-// # siot connector
-//
-
 var siot = module.exports = {};
 
-// Linear Regression
 siot.gateway = require('./src/siot_gateway');
 siot.sensor = require('./src/siot_sensor');
 siot.actor = require('./src/siot_actor');
